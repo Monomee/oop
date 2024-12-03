13881835061653aa750e33ac5d8c747a9564d2f2
Monomee <hoangthth12@gmail.com>
2024-12-03 20:59:09 +0700

    fan

---
+++ Fan/Fan/Fan.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fan
+{
+    public class Fan
+    {
+        private const int MEDIUM = 2;
+        private const int FAST = 3;
+        private int SLOW = 1;
+        //private int speed
+        //{
+        //    get => SLOW;
+        //    set
+        //    {
+        //        if (value == 2)
+        //        {
+        //            SLOW = MEDIUM;
+        //        }else if (value == 3)
+        //        {
+        //            SLOW = FAST;
+        //        }
+        //    }
+        //}
+        private int speed = 1;
+        private bool on = false;
+        private double radius = 5;
+        private string color = "blue";
+
+        public bool On { get => on; set => on = value; }
+        public double Radius { get => radius; set => radius = value; }
+        public string Color { get => color; set => color = value; }
+        public int Speed { get => speed; set => speed = value; }
+
+        public Fan()
+        {
+        }
+
+        public override string ToString()
+        {
+            if (on)
+            {
+                return "Speed: " + speed + ", Color: " + color + ", Radius: " + radius + ", fan is on.";
+            }
+            else
+            {
+                return "Color: " + color + ", Radius: " + radius + ", fan is off.";
+            }
+        }
+    }
+}
---
+++ Fan/Fan/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fan fan1 = new Fan();
+            Fan fan2 = new Fan();
+
+            fan1.Speed = 3;
+            fan1.Radius = 10;
+            fan1.Color = "yellow";
+            fan1.On = true;
+
+            fan2.Speed = 2;
+            fan2.Radius = 5;
+            //fan2.Color = "blue";
+            //fan2.On = false;
+
+            Console.WriteLine(fan1.ToString());
+            Console.WriteLine(fan2.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
